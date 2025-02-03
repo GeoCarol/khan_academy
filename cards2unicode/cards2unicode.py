7b958685074c8bfa783b97bbb7cfdf54bb16180b
GeoCarol <46351618+GeoCarol@users.noreply.github.com>
2025-02-03 21:46:17 +0000

    Added library

---
+++ cards2unicode/cards2unicode.py
@@ -0,0 +1,27 @@
+import sys
+
+if sys.version_info.major == 3:
+    unichr = chr
+
+FACES = 'FBA23456789TJCQK'
+UNICODE_FACES = 'F0123456789ABCDE'
+SUITS = 'SHDC'
+UNICODE_SUITS = 'ABCD'
+COLORS = ['\x1b[%dm' % c for c in (30, 31, 34, 32, 39)]
+
+
+def unicard(card, color=False):
+    if card[:2] == '10':
+        card = 'T' + card[2]
+    # B for playing card back
+    if card[:1].upper() == 'B':
+        face, suit = 'BS'
+    # F for fool aka joker
+    if card[:1].upper() == 'F':
+        face, suit = 'FC'
+    else:
+        face, suit = card.upper()
+    c = unichr(int('0001f0%s%s' % (
+        UNICODE_SUITS[SUITS.index(suit)],
+        UNICODE_FACES[FACES.index(face)]
+    ), base=16))
